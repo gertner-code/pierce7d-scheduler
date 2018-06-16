7c9f1e1b94e3f0deb2bc994b235333c26fd8ec33
jgertner <gertner.code@gmail.com>
2018-06-16 18:42:24 -0400

    feat: Added serialize for both tables

--- database_setup.py
+++ database_setup.py
@@ -18,6 +18,17 @@ class User(Base):
     games = Column(postgresql.ARRAY(String(40), dimensions=4), nullable=True)
     country = Column(String(60), nullable=False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily readable format."""
+        return {
+            'id': self.id,
+            'email': self.email,
+            'discord_username': self.discord_username,
+            'games': self.games,
+            'country': self.country,
+        }
+
 
 class Event(Base):
     """docstring for Event."""
@@ -28,6 +39,17 @@ class Event(Base):
     time = Column(TSRANGE())  # timestamp range
     comment = Column(String(250), nullable=True)
     game = Column(String(40), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily readable format."""
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'time': self.time,
+            'comment': self.comment,
+            'game': self.game,
+        }
 
 engine = create_engine('postgresql://pierce7d.db')
 
