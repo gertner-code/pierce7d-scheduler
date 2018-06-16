092ba46364de1c3d5b1d23edc51e1d2e70088ccf
jgertner <gertner.code@gmail.com>
2018-06-16 17:56:11 -0400

    feat: User table created

--- database_setup.py
+++ database_setup.py
@@ -4,3 +4,13 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    discord_username = Column(String(40), nullable=False)
+    games = Column(postgresql.ARRAY(String(25), dimensions=4), nullable=True)
+    country = Column(String(60), nullable=False)
+    
